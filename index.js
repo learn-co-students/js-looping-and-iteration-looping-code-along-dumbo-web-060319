cfe2ffa2faa6090c77fe970e77a5450f2bee5b76
Kevin Lai <kevinlai76@gmail.com>
2019-07-14 15:53:22 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+    const thankYou = []
+
+    for (let i = 0; i < names.length; i++) {
+        let thankYouMessage = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`
+        thankYou.push(thankYouMessage)
+    }
+
+    return thankYou
+}
+
+
+function countdown(integer) {
+    while (integer >= 0) {
+        console.log(integer);
+        integer -= 1
+    }
+}
