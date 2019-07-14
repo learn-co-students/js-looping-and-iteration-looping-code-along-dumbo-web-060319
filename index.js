f6fb4a5bd9c0f2b747f5e7fe4654bd36000c71d7
Alexander Schelchere <ascellie@gmail.com>
2019-07-14 11:32:38 -0400

    loops

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const writeCards = names => {
+  let cards = [];
+
+  for (let i = 0; i < names.length; i++) {
+    let greeting = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+    cards.push(greeting);
+  }
+
+  return cards;
+};
+
+const countdown = num => {
+  let count = num;
+  while (count >= 0) {
+    console.log(count);
+    count--;
+  }
+};
