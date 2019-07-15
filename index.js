ff89300f2fa203d78f51cae3b6c0c1124b4b71b7
github username <github email address>
2019-07-15 16:55:52 -0400

    variable declarations in JS are much more explicit than Ruby.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names, event){
+    let storage = [];
+    for (let i = 0; i < names.length; i++){
+        storage.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return storage;
+}
+
+function countdown(num){
+    let count = 0;
+    while (num >= count) {
+        console.log(num);
+        num--;
+    }
+}
