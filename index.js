0c4230877232f298477109c2c2ac39ea2c21e5c9
Hamza  Javed <hamzajaved71@gmail.com>
2019-07-15 20:50:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(arr){
+  let a = [];
+  for (let i=0; i <= (arr.length)-1 ; i++ ){
+    
+  a.push( (`Thank you, ${arr[i]}, for the wonderful surprise gift!`));
+  }
+  return a;
+}
+  
+function countdown(num){
+  while (num >= 0){
+    console.log(num);
+    num --;
+  }
+  
+}
+
